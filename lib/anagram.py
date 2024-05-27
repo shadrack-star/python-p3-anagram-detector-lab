c0b107e9f400c6566655ab8c59c2fe9a00de1add
shadrack-star <shaddkiplangat98@gmail.com>
2024-05-27 08:38:27 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+# lib/anagram.py
+class Anagram:
+    def __init__(self,word):
+        self.word=word.lower()
+    
+    def is_anagram(self, other_word):
+        return sorted(self.word) == sorted(other_word.lower())
+    
+    def match(self,word_list):
+        return [word for word in word_list if self.is_anagram(word)]
--- lib/debug.py
+++ lib/debug.py
@@ -2,5 +2,6 @@
 
 from anagram import Anagram
 
+
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
